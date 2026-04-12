63bc8511700b3fad4877c7bd896466165d2ea813
NSToki <mdnstoki@gmail.com>
2026-04-12 22:15:23 +0600

    php Learning Start

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php Tutorial</title>
+</head>
+<body>
+    <div class="container">
+        <h1>This is my PHP Learning</h1>
+        <h1>This is my PHP Learning</h1>
+        <h1>This is my PHP Learning</h1>
+    </div>
+
+<?php 
+// This is single line comment by php way
+
+/**
+ * this
+ * is multiple
+ * line comment
+ */
+
+
+$variable1 = 50;
+$variable2 = 100;
+
+echo $variable1 + $variable2;
+?>
+
+    
+</body>
+</html>
