5c3121a6dfa1f10c77220c017bc1f53a6f222937
NSToki <mdnstoki@gmail.com>
2026-04-27 21:42:49 +0600

    Function Done

---
+++ Phudu/NavBar.php
@@ -0,0 +1,17 @@
+
+<nav>
+  <div>
+    <div>
+<h1>Hello</h1>
+    </div>
+    <div>
+        <div>Home</div>
+        <div>My-Booking</div>
+        <div>Blogs</div>
+        <div>Contact Us</div>
+    </div>
+    <div>
+        Emergency
+    </div>
+  </div>
+</nav>
---
+++ Phudu/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en" data-theme="light">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Phudu</title>
+</head>
+
+<body>
+
+<?php include "NavBar.php"; ?>
+
+</body>
+</html>
